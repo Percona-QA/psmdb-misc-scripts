98038150d2d4025c2c1f1548391c9e1b4a3d4577
David Bennett <david.bennett@percona.com>
2016-01-26 02:50:58 +0000

    Convert TokuMX oplog GTID in BinData(0) base64 format to the format required for the toku2mongo tool.

---
+++ BinData_to_GTID.js
@@ -0,0 +1,19 @@
+var gtidHexString='';
+
+db=db.getSiblingDB('local');
+
+db.oplog.rs.find().sort({$natural:-1}).limit(1).forEach(function(o){
+  gtidHexString = o._id.hex();
+});
+
+if (gtidHexString == '') {
+  print("GTID not found.");
+  quit();
+}
+
+var uuid=gtidHexString.substring(16);
+var seq=gtidHexString.substring(0,16);
+
+print(gtidHexString);
+print('0x'+uuid+':'+'0x'+seq);
+
