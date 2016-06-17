cc38fc7c9254f6fa3661c186fc6746786f9a240e
David Bennett <david.bennett@percona.com>
2016-06-17 20:56:25 +0000

    Updated PSMDB 3.0 smoke test running script
    - Added missing smoke options from upstream configurations
    - Added ability to run pre-smoke setup commands
    - Added summary report generation

--- run_smoke_psmdb_3.0.sh
+++ run_smoke_psmdb_3.0.sh
@@ -16,40 +16,47 @@
 
 # suite name or runSets can have -- style options specified 
 # which are passed to smoke
+#
+# lines beginning one or more spaces followed by a $ denote shell commands to
+# run before the preceding suite runSets are run.
 
-readarray SUITES <<-'EOS'
-aggregation,default,auth,se
-auth,default,se
-concurrency,default,se
-concurrency_compatibility,default,se
-dbtest,default,se
-disk,default
-durability,default
-failpoints,default,auth
-gle_auth,default,se
-gle_auth_write_cmd,default,se
-jsCore,se,default,auth
-jsCore_compatibility,default,se
-jsCore_small_oplog,default,se
-mmap,default
-mongosTest,default
-multiversion,default
-noPassthrough,default,se	
-noPassthroughWithMongod,se
-noPassthrough,se
-parallel,default,se	
-parallel_compatibility,default,se
-replicasets,default,se,auth
-replication,default,se,auth
-sharding,default,se,auth
-slow1,default,se
-ssl --use-ssl,default
-sslSpecial,default
-tool,default,se
+readarray SUITES <<-EOS
+aggregation --nopreallocj,default,auth,se
+audit --nopreallocj,default,se
+auth --nopreallocj,default,se
+concurrency --nopreallocj,default,se
+concurrency --nopreallocj --shell-write-mode compatibility,default,se
+dbtest --nopreallocj,default,se
+disk --nopreallocj,default
+dur --nopreallocj,default
+failPoint --nopreallocj,default,auth
+gle --nopreallocj --auth,default,se
+gle --nopreallocj --auth --shell-write-mode commands,default,se
+jsCore --nopreallocj --shell-write-mode commands,se,default,auth
+jsCore --nopreallocj --shell-write-mode compatibility,default,se
+jsCore --shell-write-mode commands --small-oplog,default,se
+mmap_v1 --nopreallocj,default
+mongosTest --nopreallocj,default
+multiVersion --nopreallocj,default
+  $ rm -rf ${DBPATH}/install ${DBPATH}/multiversion
+  $ python buildscripts/setup_multiversion_mongodb.py ${DBPATH}/install ${DBPATH}/multiversion "Linux/x86_64" "1.8" "2.0" "2.2" "2.4" "2.6"
+  $ [[ ${PATH} == *"/data/multiversion"* ]] || export PATH=${PATH}:/data/multiversion 
+noPassthrough --nopreallocj,default,se	
+noPassthroughWithMongod --nopreallocj,default,se
+parallel --nopreallocj,default,se	
+parallel --nopreallocj --shell-write-mode compatibility,default,se	
+replSets --nopreallocj,default,se,auth
+repl --nopreallocj,default,se,auth
+sharding --nopreallocj,default,se,auth
+slow1 --nopreallocj,default,se
+slow2 --nopreallocj,default,se
+ssl --nopreallocj --use-ssl,default
+sslSpecial --nopreallocj,default
+tool --nopreallocj,default,se
 EOS
 
 # smoke parameters
-SMOKE_BASE="--continue-on-failure --clean-every=1"
+SMOKE_BASE="--continue-on-failure --with-cleanbb"
 SMOKE_DEFAULT=""
 SMOKE_AUTH="--auth"
 SMOKE_SE=""
@@ -122,8 +129,52 @@ done
 runSmoke() {
   local smokeParams=$1
   local logOutputFile=$2
-  # shellcheck disable=SC2086
-  python buildscripts/smoke.py ${smokeParams} 2>&1 | tee "${logOutputFile}"
+  local suiteRawName=$3
+  local foundCommandSuite=false
+
+  rm -rf ${DBPATH}/db/*
+
+  # look for preprocessing commands
+
+  (
+    for suiteCommand in "${SUITES[@]}"; do
+
+      if ! ${foundCommandSuite} && [[ "${suiteCommand}" =~ ^[[:space:]]*\$ ]]; then
+        continue
+      fi
+
+      if ${foundCommandSuite}; then
+        if [[ "${suiteCommand}" =~ ^[[:space:]]*\$ ]]; then
+          # shellcheck disable=SC2001
+          preprocessingCommand=$(echo "${suiteCommand}" | sed 's/^\s*\$//')
+          echo "Running: ${preprocessingCommand}" | tee -a "${logOutputFile}"
+          # if export builtin is piped it will fail
+          if [[ "${preprocessingCommand}" == *export* ]]; then
+            eval "${preprocessingCommand}"
+          else
+            eval "${preprocessingCommand}" 2>&1 | tee -a "${logOutputFile}"
+          fi
+          continue
+        else
+          break
+        fi
+      fi
+
+      IFS=',' read -r -a suiteDefinition <<< "${suiteCommand}"
+      suiteName="${suiteDefinition[0]}"
+      if [ "${suiteName}" = "${suiteRawName}" ]; then
+        foundCommandSuite=true
+        continue
+      else
+        foundCommandSuite=false
+      fi
+
+    done
+
+    echo "Running Command: python buildscripts/smoke.py ${smokeParams}" | tee -a "${logOutputFile}"
+    # shellcheck disable=SC2086
+    python buildscripts/smoke.py ${smokeParams} 2>&1 | tee -a "${logOutputFile}"
+  )
 }
 
 hasEngine() {
@@ -133,8 +184,15 @@ hasEngine() {
 }
 
 for suite in "${SUITES[@]}"; do
+
+  # skip lines begining with space
+  if [[ "${suite}" == " "* ]]; then
+    continue; 
+  fi
+
   IFS=',' read -r -a suiteDefinition <<< "${suite}"
   suiteElementNumber=0
+
   for suiteElement in "${suiteDefinition[@]}"; do
     if [[ ${suiteElement} =~ ^([a-zA-Z0-9_]+)[[:space:]](.*)$ ]]; then
       suiteElementName=${BASH_REMATCH[1]}
@@ -145,24 +203,26 @@ for suite in "${SUITES[@]}"; do
     fi
     if [ ${suiteElementNumber} -eq 0 ]; then
       suite=${suiteElementName}
+      suiteRawName=${suiteElement}
       suiteOptions=${suiteElementOptions}
+      suiteLogTag=$(echo "${suiteElement}" | sed -r -e 's/ +/_/g' -e 's/-//g')
     else
       suiteRunSet=${suiteElementName}
       suiteRunSetOptions=${suiteElementOptions}
-      logOutputFilePrefix="smoke_${suite}_${suiteRunSet}"
+      logOutputFilePrefix="smoke_${suiteLogTag}_${suiteRunSet}"
       case "$suiteRunSet" in
         "default"|"auth")
           logOutputFile="${logOutputFilePrefix}_${trial}.log"
           [ "${suiteRunSet}" == "default" ] && smokeParams=${SMOKE_DEFAULT}
           [ "${suiteRunSet}" == "auth" ] && smokeParams=${SMOKE_AUTH}
           smokeParams="${SMOKE_BASE} ${smokeParams} --storageEngine=${DEFAULT_ENGINE} ${suiteOptions} ${suiteRunSetOptions} ${suite}"
-          runSmoke "${smokeParams}" "$logOutputFile"
+          runSmoke "${smokeParams}" "${logOutputFile}" "${suiteRawName}"
           ;;
         "wiredTiger"|"PerconaFT"|"rocksdb"|"mmapv1")
           logOutputFile="${logOutputFilePrefix}_${trial}.log"
           if hasEngine "${suiteRunSet}"; then
             smokeParams="${SMOKE_BASE} ${SMOKE_SE} --storageEngine=${suiteRunSet} ${suiteOptions} ${suiteRunSetOptions} ${suite}"
-            runSmoke "${smokeParams}" "$logOutputFile"
+            runSmoke "${smokeParams}" "${logOutputFile}" "${suiteRawName}"
           else
             echo "failed: Storage Engine runSet: ${suiteRunSet} requested for suite ${suite} but is not available." | tee -a "${logOutputFile}"
           fi
@@ -172,7 +232,7 @@ for suite in "${SUITES[@]}"; do
             if [ ! "${engine}" == "${DEFAULT_ENGINE}" ]; then
               logOutputFile="${logOutputFilePrefix}_${engine}_${trial}.log"
               smokeParams="${SMOKE_BASE} --storageEngine=${engine} ${SMOKE_SE} ${suiteOptions} ${suiteRunSetOptions} ${suite}"
-              runSmoke "${smokeParams}" "$logOutputFile"
+              runSmoke "${smokeParams}" "${logOutputFile}" "${suiteRawName}"
             fi
           done
           ;;
@@ -184,4 +244,12 @@ for suite in "${SUITES[@]}"; do
     ((suiteElementNumber++))
   done
 done
+
+# output summary report
+
+find . -type f -name '*_${trial}.log' \
+  -print \
+  -exec sh -c 'logfile="$1"; grep -E -A9999 "^[0-9]+ tests succeeded" "${logfile}" | sed -e "s/^/    /"' _ "{}" \; \
+  -exec echo "" \; \
+  | tee "smoke_summary_${trial}.log"
 
