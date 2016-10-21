afd04a78a638221e79ed5f2df3678dc592d19f6d
Denis Protyvenskyi <denis.protyvenskyi@percona.com>
2016-10-21 15:40:47 +0300

    Add MONGOTOOLS env var as a path to search for mongo-tools
    Defaults to the working dir.

--- run_resmoke_psmdb_3.2.sh
+++ run_resmoke_psmdb_3.2.sh
@@ -51,9 +51,13 @@ if [ "$1" == "" ]; then
   echo -e "\t\tdefault suite set is: resmoke_psmdb_3.2_default.txt"
   echo -e "\t\tif the suite set is not found in psmdb-misc-scripts/suite_sets"
   echo -e "\t\tthen script will attempt to load as absolute path."
+  echo -e "Env vars:"
+  echo -e "\t\tMONGOTOOLS - path to mongo-tools, defaults to the working directory."
   exit 1;
 fi
 trial=$1
+
+: ${MONGOTOOLS:=.}
 
 run_system_validations
 
