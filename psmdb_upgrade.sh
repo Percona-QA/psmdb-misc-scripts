f77ca8878962d6831da8d7e74dfa472a9fb96b2e
Tomislav Plavcic <tomislav.plavcic@percona.com>
2018-04-10 11:29:21 +0200

    Fix for psmdb_upgrade.sh

--- psmdb_upgrade.sh
+++ psmdb_upgrade.sh
@@ -223,9 +223,9 @@ start_replica()
 init_replica()
 {
   ${PSMDB_OLD_BINDIR}/bin/mongo --host=${HOST} --port ${NODE1_PORT} --eval "rs.initiate({ _id: \"rs0\", members: [ { _id: 0, host: \"${HOST}:${NODE1_PORT}\" } ] })"
+  sleep 30
   ${PSMDB_OLD_BINDIR}/bin/mongo --host=${HOST} --port ${NODE1_PORT} --eval "rs.add(\"${HOST}:${NODE2_PORT}\")"
   ${PSMDB_OLD_BINDIR}/bin/mongo --host=${HOST} --port ${NODE1_PORT} --eval "rs.add(\"${HOST}:${NODE3_PORT}\")"
-  sleep 30
 }
 
 import_test_data()
