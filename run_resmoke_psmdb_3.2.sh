6d3e4612576dc5caaa67c908cccc7362aadcf711
Igor Solodovnikov <igor.solodovnikov@percona.com>
2018-11-06 18:37:08 +0200

    PQA-311 add default excluded tags per SE

--- run_resmoke_psmdb_3.2.sh
+++ run_resmoke_psmdb_3.2.sh
@@ -41,6 +41,13 @@ RESMOKE_DEFAULT=""
 RESMOKE_AUTH="--auth"
 RESMOKE_SE=""
 
+# default tags to exclude per SE
+RESMOKE_EXCLUDE_wiredTiger="--excludeWithAnyTags=requires_mmapv1"
+RESMOKE_EXCLUDE_PerconaFT=""
+RESMOKE_EXCLUDE_rocksdb=""
+RESMOKE_EXCLUDE_mmapv1="--excludeWithAnyTags=requires_wiredtiger,uses_transactions,requires_document_locking"
+RESMOKE_EXCLUDE_inMemory="--excludeWithAnyTags=requires_persistence,requires_journaling,requires_mmapv1,uses_transactions"
+
 run_system_validations
 
 # trial number
@@ -161,7 +168,8 @@ for suite in "${SUITES[@]}"; do
           echo "-----------------" | tee -a "${logOutputFile}"
           echo "Suite Definition: ${suiteRawName}${suiteOptions:+ ${suiteOptions}}|${suiteElement}" | tee -a "${logOutputFile}"
           if hasEngine "${suiteRunSet}"; then
-            resmokeParams="${RESMOKE_BASE} ${RESMOKE_SE} --storageEngine=${suiteRunSet} ${suiteOptions} ${suiteRunSetOptions}"
+            excludeTagsVar="RESMOKE_EXCLUDE_${suiteRunSet}"
+            resmokeParams="${RESMOKE_BASE} ${RESMOKE_SE} --storageEngine=${suiteRunSet} ${!excludeTagsVar} ${suiteOptions} ${suiteRunSetOptions}"
             if $useSuitesOption; then
               resmokeParams="${resmokeParams} --suites=${suite}"
             fi
