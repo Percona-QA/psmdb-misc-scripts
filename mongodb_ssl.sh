fb5332bd2e0b497af68ab15f2a70dafcd9988f68
Mikhail Samoylov <mikhail.samoylov@percona.com>
2019-07-03 23:33:10 +0300

    Add default value for email or set it via $1

--- mongodb_ssl.sh
+++ mongodb_ssl.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+if [ "$1" != "" ]; then
+	EMAIL=$1
+else
+	EMAIL="tomislav.plavcic@percona.com"
+fi
 
 # Generate self signed root CA cert
-openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj "/C=US/ST=California/L=San Francisco/O=Percona/OU=root/CN=`hostname -f`/emailAddress=tomislav.plavcic@percona.com"
+openssl req -nodes -x509 -newkey rsa:2048 -keyout ca.key -out ca.crt -subj "/C=US/ST=California/L=San Francisco/O=Percona/OU=root/CN=`hostname -f`/emailAddress=$EMAIL"
 
 # Generate server cert to be signed
-openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/C=US/ST=California/L=San Francisco/O=Percona/OU=server/CN=`hostname -f`/emailAddress=tomislav.plavcic@percona.com"
+openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/C=US/ST=California/L=San Francisco/O=Percona/OU=server/CN=`hostname -f`/emailAddress=$EMAIL"
 
 # Sign the server cert
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
@@ -13,7 +18,7 @@ openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out s
 cat server.key server.crt > server.pem
 
 # Generate client cert to be signed
-openssl req -nodes -newkey rsa:2048 -keyout client.key -out client.csr -subj "/C=US/ST=California/L=San Francisco/O=Percona/OU=client/CN=`hostname -f`/emailAddress=tomislav.plavcic@percona.com"
+openssl req -nodes -newkey rsa:2048 -keyout client.key -out client.csr -subj "/C=US/ST=California/L=San Francisco/O=Percona/OU=client/CN=`hostname -f`/emailAddress=$EMAIL"
 
 # Sign the client cert
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAserial ca.srl -out client.crt
